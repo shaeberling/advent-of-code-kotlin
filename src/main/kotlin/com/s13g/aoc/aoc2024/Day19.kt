9976b55f951365fbd76d7cf05fd838bafcbf2f0e
Sascha Haeberling <mail@sascha-haeberling.com>
2024-12-19 15:38:02 -0800

    AoC 2024.19 in Kotlin

--- src/main/kotlin/com/s13g/aoc/aoc2024/Day19.kt
+++ src/main/kotlin/com/s13g/aoc/aoc2024/Day19.kt
@@ -4,11 +4,32 @@ import com.s13g.aoc.Result
 import com.s13g.aoc.Solver
 
 /**
- * --- Day 19:  ---
+ * --- Day 19: Linen Layout ---
  * https://adventofcode.com/2024/day/19
  */
-class Day19 : Solver{
+class Day19 : Solver {
   override fun solve(lines: List<String>): Result {
-    TODO("Not yet implemented")
+    val avail = lines[0].split(",").map { it.trim() }
+    val desired = lines.subList(2, lines.size)
+
+    val result1 = desired.count { countVariation(it, avail) > 0 }
+    val result2 = desired.sumOf { countVariation(it, avail) }
+    return Result("$result1", "$result2")
+  }
+
+  val possibleCache = mutableMapOf<String, Long>()
+  private fun countVariation(pattern: String, avail: List<String>): Long {
+    if (pattern in possibleCache) return possibleCache[pattern]!!
+
+    var variations = 0L
+    for (a in avail.toSet()) {
+      if (a == pattern) variations++
+      if (pattern.startsWith(a)) {
+        val subPattern = pattern.substring(a.length)
+        variations += countVariation(subPattern, avail)
+      }
+    }
+    possibleCache[pattern] = variations
+    return variations
   }
 }
